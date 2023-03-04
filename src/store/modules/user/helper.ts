f9ac931e0ead211d2c301c0d88c8bd462c676c46
MGNB <50907416+MGNB-ss@users.noreply.github.com>
2023-03-04 10:11:15 +0900

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -17,7 +17,7 @@ export function defaultSetting(): UserState {
     userInfo: {
       avatar: 'https://s2.loli.net/2023/03/04/24wZfFpX9jaClKE.jpg',
       name: 'MGNB',
-      description: '欢迎使用 <a href="https://mgnb.fun" class="text-blue-500" target="_blank" >MGNB</a> 翻墙',
+      description: '欢迎使用 <a href="https://mgnb.fun" class="text-blue-500" target="_blank" >MGNB</a> 网络加速',
     },
   }
 }
