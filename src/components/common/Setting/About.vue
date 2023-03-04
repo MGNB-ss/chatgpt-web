216fed81cdfe8d9fee22d70de8af245a262c6f5d
MGNB <50907416+MGNB-ss@users.noreply.github.com>
2023-03-04 10:09:44 +0900

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -39,18 +39,18 @@ onMounted(() => {
       </h2>
       <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
         <p>
-          此项目开源于
+          此项目由
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
+            href="https://mgnb.fun"
             target="_blank"
           >
-            Github
+            MGNB
           </a>
-          ，免费且基于 MIT 协议，没有任何形式的付费行为！
+          独家赞助，免费但请勿滥用！
         </p>
         <p>
-          如果你觉得此项目对你有帮助，请在 Github 帮我点个 Star 或者给予一点赞助，谢谢！
+          如果你觉得此项目对你有帮助，请购买我们的服务或者给予一点赞助，谢谢！
         </p>
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
